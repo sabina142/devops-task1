8499ff1d0ea64714434f7a4192eb442118944198
sabina142 <sabinaprodduturi86@gmail.com>
2026-06-07 19:27:25 +0530

    add cost tagging to terraform resources

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const helmet = require("helmet");
+
 const app = express();
+
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.send("Hello DevOps 🚀");
+});
+
+app.get("/predict", (req, res) => {
+  res.json({
+    prediction: "sample_prediction",
+    confidence: 0.95,
+    status: "success"
+  });
 });
 
 app.listen(3000, () => {
